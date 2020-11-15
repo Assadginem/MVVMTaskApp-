4fb64620f9b6bfa4cd59dd204759647dc3ca60c5
assadginem <assad.ginem@smartbear.com>
2020-11-14 20:07:16 -0600

    Finished entity class

--- app/src/main/java/com/ginem/mvvmtaskapp/Note.java
+++ app/src/main/java/com/ginem/mvvmtaskapp/Note.java
@@ -1,7 +1,47 @@
 package com.ginem.mvvmtaskapp;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "note_table")
 public class Note {
+
+    // Each SQLite table needs a primary key so we can uniquely identify each entry
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+    
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+
 }
